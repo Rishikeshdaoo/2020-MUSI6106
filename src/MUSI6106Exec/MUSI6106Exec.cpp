5a7f9307bc31fc0cf623d7c77bf1a4d374fb9a68
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-01-12 23:56:05 -0500

    commit assign1: file read

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <fstream>
 #include <ctime>
 
 #include "MUSI6106Config.h"
@@ -33,21 +34,52 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
- 
-    //////////////////////////////////////////////////////////////////////////////
-    // open the input wave file
- 
-    //////////////////////////////////////////////////////////////////////////////
-    // open the output text file
- 
-    //////////////////////////////////////////////////////////////////////////////
-    // allocate memory
- 
-    //////////////////////////////////////////////////////////////////////////////
-    // get audio data and write it to the output text file (one column per channel)
+
+    sInputFilePath = "/Users/marketinggramusic/Documents/Sem2/AudioSoftwareEngg/sweep.wav";
+    sOutputFilePath = "/Users/marketinggramusic/Documents/Sem2/AudioSoftwareEngg/sweep.txt";
 
     //////////////////////////////////////////////////////////////////////////////
+    // open the input wave file
+    
+    stFileSpec.eFormat = CAudioFileIf::FileFormat_t::kFileFormatWav;
+    stFileSpec.eBitStreamType = CAudioFileIf::BitStream_t::kFileBitStreamInt16;
+    stFileSpec.iNumChannels = 2;
+    stFileSpec.fSampleRateInHz = 44100;
+    
+    CAudioFileIf::create(phAudioFile);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::FileIoType_t::kFileRead, &stFileSpec);
+
+    //////////////////////////////////////////////////////////////////////////////
+    // open the output text file
+    hOutputFile.open(sOutputFilePath, std::fstream::out);
+
+    //////////////////////////////////////////////////////////////////////////////
+    // allocate memory
+    int sizeOfBuffer = sizeof(float) * kBlockSize;
+    ppfAudioData = (float**) malloc(sizeof(float) * 2);
+    *ppfAudioData = (float*) malloc(sizeOfBuffer);
+    *(ppfAudioData+1) = (float*) malloc(sizeOfBuffer);
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // get audio data and write it to the output text file (one column per channel)
+    long long fileData = sizeOfBuffer;
+    
+    while(fileData!=0){
+        phAudioFile->readData(ppfAudioData, fileData);
+        
+        for(int buf =0; buf<fileData; buf++){
+            hOutputFile << ppfAudioData[0][buf] << "\t" << ppfAudioData[1][buf] <<endl;
+        }
+
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////
     // clean-up (close files and free memory)
+    delete *ppfAudioData;
+    delete *(ppfAudioData+1);
+    delete ppfAudioData;
+    CAudioFileIf::destroy(phAudioFile);
+    hOutputFile.close();
 
     // all done
     return 0;
