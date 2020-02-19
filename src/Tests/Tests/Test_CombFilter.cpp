995b0ca4973bbdf97c2976fc51dfcd3ba2514bb1
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-02-19 15:12:25 -0500

    Revert "Merge branch 'master' of https://github.com/alexanderlerch/2020-MUSI6106 into ass2_stub"
    This reverts commit a248040801a2ea97c62261cea22d2c2e7178e85e, reversing
    changes made to a6372457896da249887c089f9080905efb9a9b9b.

--- src/Tests/Tests/Test_CombFilter.cpp
+++ src/Tests/Tests/Test_CombFilter.cpp
@@ -264,13 +264,14 @@ SUITE(CombFilter)
         for (int c = 0; c < m_iNumChannels; c++)
             CHECK_ARRAY_CLOSE(m_ppfInputData[c], m_ppfOutputData[c], m_iDataLength, 1e-3);
     }
+
     TEST_FIXTURE(CombFilterData, VaryingBlocksize)
     {
         //Fir
         m_pCombFilter->init(CCombFilterIf::kCombFIR, m_fMaxDelayLength, m_fSampleRate, m_iNumChannels);
 
         for (int c = 0; c < m_iNumChannels; c++)
-            CSynthesis::generateSine(m_ppfInputData[c], 387.F, m_fSampleRate, m_iDataLength, .8F, static_cast<float>(c * M_PI_2));
+            CSynthesis::generateSine (m_ppfInputData[c], 387.F, m_fSampleRate, m_iDataLength, .8F, static_cast<float>(c*M_PI_2));
         m_pCombFilter->setParam(CCombFilterIf::kParamGain, m_fGain);
         m_pCombFilter->setParam(CCombFilterIf::kParamDelay, m_fDelay);
 
@@ -280,25 +281,18 @@ SUITE(CombFilter)
         m_pCombFilter->init(CCombFilterIf::kCombFIR, m_fMaxDelayLength, m_fSampleRate, m_iNumChannels);
         m_pCombFilter->setParam(CCombFilterIf::kParamGain, m_fGain);
         m_pCombFilter->setParam(CCombFilterIf::kParamDelay, m_fDelay);
-
-        float** ppfTmp = new float* [m_iNumChannels];
-        for (int c = 0; c < m_iNumChannels; c++)
-            ppfTmp[c] = new float[17000];
         {
             int iNumFramesRemaining = m_iDataLength;
-
             while (iNumFramesRemaining > 0)
             {
 
-                int iNumFrames = std::min(static_cast<float>(iNumFramesRemaining), static_cast<float>(rand()) / RAND_MAX * 17000.F);
+                int iNumFrames = std::min(static_cast<float>(iNumFramesRemaining), static_cast<float>(rand())/RAND_MAX*17000.F);
 
                 for (int c = 0; c < m_iNumChannels; c++)
                 {
-                    m_ppfInputTmp[c] = &m_ppfInputData[c][m_iDataLength - iNumFramesRemaining];
+                    m_ppfInputTmp[c]    = &m_ppfInputData[c][m_iDataLength - iNumFramesRemaining];
                 }
-                m_pCombFilter->process(m_ppfInputTmp, ppfTmp, iNumFrames);
-                for (int c = 0; c < m_iNumChannels; c++)
-                    CVectorFloat::copy(m_ppfInputTmp[c], ppfTmp[c], iNumFrames);
+                m_pCombFilter->process(m_ppfInputTmp, m_ppfInputTmp, iNumFrames);
 
                 iNumFramesRemaining -= iNumFrames;
             }
@@ -311,7 +305,7 @@ SUITE(CombFilter)
         m_pCombFilter->init(CCombFilterIf::kCombIIR, m_fMaxDelayLength, m_fSampleRate, m_iNumChannels);
 
         for (int c = 0; c < m_iNumChannels; c++)
-            CSynthesis::generateSine(m_ppfInputData[c], 387.F, m_fSampleRate, m_iDataLength, .8F, static_cast<float>(c * M_PI_2));
+            CSynthesis::generateSine (m_ppfInputData[c], 387.F, m_fSampleRate, m_iDataLength, .8F, static_cast<float>(c*M_PI_2));
         m_pCombFilter->setParam(CCombFilterIf::kParamGain, m_fGain);
         m_pCombFilter->setParam(CCombFilterIf::kParamDelay, m_fDelay);
 
@@ -326,15 +320,13 @@ SUITE(CombFilter)
             while (iNumFramesRemaining > 0)
             {
 
-                int iNumFrames = std::min(static_cast<float>(iNumFramesRemaining), static_cast<float>(rand()) / RAND_MAX * 17000.F);
+                int iNumFrames = std::min(static_cast<float>(iNumFramesRemaining), static_cast<float>(rand())/RAND_MAX*17000.F);
 
                 for (int c = 0; c < m_iNumChannels; c++)
                 {
-                    m_ppfInputTmp[c] = &m_ppfInputData[c][m_iDataLength - iNumFramesRemaining];
+                    m_ppfInputTmp[c]    = &m_ppfInputData[c][m_iDataLength - iNumFramesRemaining];
                 }
-                m_pCombFilter->process(m_ppfInputTmp, ppfTmp, iNumFrames);
-                for (int c = 0; c < m_iNumChannels; c++)
-                    CVectorFloat::copy(m_ppfInputTmp[c], ppfTmp[c], iNumFrames);
+                m_pCombFilter->process(m_ppfInputTmp, m_ppfInputTmp, iNumFrames);
 
                 iNumFramesRemaining -= iNumFrames;
             }
@@ -342,10 +334,6 @@ SUITE(CombFilter)
 
         for (int c = 0; c < m_iNumChannels; c++)
             CHECK_ARRAY_CLOSE(m_ppfInputData[c], m_ppfOutputData[c], m_iDataLength, 1e-3);
-
-        for (int c = 0; c < m_iNumChannels; c++)
-            delete[]ppfTmp[c];
-        delete[] ppfTmp;
     }
 }
 
