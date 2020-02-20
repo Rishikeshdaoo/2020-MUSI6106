da98bcd12710a1d28e5c9f14ca39017012199288
rishikeshdaoo <rishikesh.daoo@gmail.com>
2020-02-19 19:45:56 -0500

    Edited the Exec for FFT

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <Fft.h>
 
 #include "MUSI6106Config.h"
 
@@ -20,15 +21,20 @@ int main(int argc, char* argv[])
     std::string             sInputFilePath,                 //!< file paths
                             sOutputFilePath;
 
-    static const int        kBlockSize = 1024;
+    int                     kBlockSize,
+                            kHopSize;
 
     clock_t                 time = 0;
 
-    float                   **ppfAudioData = 0;
+    float                   **ppfAudioData = 0,
+                            *pComplexSpectrum = 0,
+                            *pBufferedInput = 0;
 
     CAudioFileIf            *phAudioFile = 0;
     std::fstream            hOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
+
+    CFft                    *pCFft;
 
     showClInfo();
 
@@ -36,11 +42,16 @@ int main(int argc, char* argv[])
     // parse command line arguments
 
     sInputFilePath = argv[1];
-    sOutputFilePath = argv[2];
+    sOutputFilePath = sInputFilePath + "output.txt";
+    kBlockSize = atoi(argv[2]);
+    kHopSize = atoi(argv[3]);
+
+    if(kHopSize > kBlockSize)
+        return -1;
 
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
-    
+
     stFileSpec.eFormat = CAudioFileIf::FileFormat_t::kFileFormatWav;
     stFileSpec.eBitStreamType = CAudioFileIf::BitStream_t::kFileBitStreamInt16;
     stFileSpec.iNumChannels = 2;
@@ -55,22 +66,41 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
-    int sizeOfBuffer = sizeof(float) * kBlockSize;
+    int sizeOfBuffer = sizeof(float) * kHopSize;
+    int sizeOfSpectrum = sizeof(float) * kBlockSize;
+
     ppfAudioData = (float**) malloc(sizeof(float) * 2);
     *ppfAudioData = (float*) malloc(sizeOfBuffer);
     *(ppfAudioData+1) = (float*) malloc(sizeOfBuffer);
-    
+
+    pBufferedInput = new float[kBlockSize]();
+    pComplexSpectrum = (float*) malloc(sizeOfSpectrum);
+
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Create FFT instance and init
+
+    CFft::createInstance(pCFft);
+    pCFft->initInstance(kBlockSize);
+
     //////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
     long long fileData = sizeOfBuffer;
     
     while(fileData!=0){
-        phAudioFile->readData(ppfAudioData, fileData);
-        
-        for(int buf =0; buf<fileData; buf++){
-            hOutputFile << ppfAudioData[0][buf] << "\t" << ppfAudioData[1][buf] <<endl;
+        phAudioFile->readData(&&pBufferedInput[kBlockSize], fileData);
+
+        for(int i=0; i<kBlockSize; i++) {
+
         }
 
+        pCFft->doFft(pComplexSpectrum, ppfAudioData[0]);
+
+    for (int buf = 0; buf < fileData; buf++) {
+//                    hOutputFile << ppfAudioData[0][buf];
+                //<< "\t" << ppfAudioData[1][buf] <<endl;
+            }
+        }
     }
     
     //////////////////////////////////////////////////////////////////////////////
